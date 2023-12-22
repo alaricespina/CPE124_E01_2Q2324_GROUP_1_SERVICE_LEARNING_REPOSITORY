73efbc937c85c466c3ed99d6dda6916dd5965611
alaricespina <alaricsespina@gmail.com>
2023-12-22 21:42:10 +0800

    1234

--- Plant Classification Models/Retrain - Existing Models.py
+++ Plant Classification Models/Retrain - Existing Models.py
@@ -9,6 +9,11 @@ from TensorflowPreTrainedModels import (
     InceptionV3Model,
     VGG19Model
 )
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import warnings 
+warnings.filterwarnings("ignore")
 
 Models = [
     EfficientNetV2B3Model(),
@@ -22,6 +27,13 @@ Models = [
     VGG19Model()
 ]
 
+if __name__ == "__main__":
+    for model in Models:
+        print(model.model_name)
+        model.prepare_data()
+        model.prepare_model()
+        model.fit_data(desired_size = 32, epochs = 25, ckpt_metric = "val_accuracy", ckpt_mode = "max")
+        print("Done Training")
 
 
 

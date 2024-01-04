cb6bbea55365d2fcf0ee08225f239c58f8f994a6
alaricespina <alaricsespina@gmail.com>
2024-01-04 22:40:19 +0800

    Home Done

--- HerbalFinder/App.js
+++ HerbalFinder/App.js
@@ -12,9 +12,9 @@ import { StatusBar } from 'expo-status-bar';
 //import { TransitionSpecs, CardStyleInterpolators } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { LinearGradient } from 'expo-linear-gradient'
-
 import MainScreen from './page_designs/MainScreen';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 
 
@@ -58,53 +58,98 @@ const MainScreenNavigator = () => {
 
 const HomeScreen_Re = () => {
   return (
-    <View className="bg-black w-full h-full">
-      // Green background
-      <View className="absolute bg-green w-full h-1/3" >
+    <View className="bg-[#090E05] w-full h-full">
+      <View className="absolute bg-green-800 w-full h-2/5 rounded-3xl" >
 
       </View>
 
-      <View className="w-full">
-        // Material Icon 
-        // Scan Icon
-        <MaterialCommunityIcons name = "menu" color="#75EE0A" size = "19"/>
-        <Text>Hi Name!</Text>
-        <MaterialCommunityIcons name = "line-scan" color = "#75EE0A" size = "19"/>
+      <View className="absolute w-80 top-16 left-1/2 -ml-40 flex-row ">
+        <View className="ml-1 mr-6 h-full items-center justify-center">
+          <MaterialCommunityIcons name="menu" size={25} color="#FFF"/>
+        </View>      
+        <Text className="h-full text-white">Hi Hatdog!</Text>
+        <View className="absolute h-full right-12 items-center justify-center">
+          <MaterialCommunityIcons name="line-scan" size={25} color="#FFF"/>
+        </View>
+        
       </View>
 
-      <View className="w-5/6">
-        <MaterialCommunityIcons name = "magnify" color ="#FFFFFF" size = "19"/>
+      <View className="absolute w-80 h-12 top-32 left-1/2 -ml-40">   
+        <View className="absolute h-full items-center justify-center ml-2">
+          <MaterialCommunityIcons  name="magnify" size={30} color="#FFF"/>
+        </View>
         <TextInput
         placeholder='Search'
         placeholderTextColor="white"
-        />        
-      </View>
-
-      <View>
-        <Text> Know more about Herbal Plants </Text>
-        <View>
-          
-        </View>
-      </View>
-
-      <View>
-        <Text> Popular Herbal Plants</Text>
-        <View>
-
-        </View>
-      </View>
-
-      <View>
-        <Text> Gallery Outer Circle </Text>
-      </View>
-
-      <View className="bg-zinc-900 w-full h-20 rounded-full">
-        <View>
-        <LinearGradient
-          colors={['rgba(0,0,0,0.8)', 'transparent']}
-          style={styles.background}
+        className = "absolute border-2 w-full h-full border-white rounded-lg pl-12 text-white"
         />
+      </View>
+
+      <View className="absolute w-80 left-1/2 -ml-40 top-52">
+        <Text className="text-white"> Know more about Herbal Plants </Text>
+      </View>
+
+      <View className="absolute w-80 left-1/2 -ml-40 top-64 h-24 flex-row">
+        <View className="w-24 bg-emerald-500 h-full rounded-lg"></View>
+        <View className="w-24 bg-emerald-600 h-full rounded-lg ml-4"></View>
+        <View className="w-24 bg-emerald-700 h-full rounded-lg ml-4"></View>
+      </View>
+
+      <View className="absolute w-80 left-1/2 -ml-40 top-96">
+        <Text className="text-white"> Popular Herbal Plants</Text>
+      </View>
+
+      <View className="absolute w-80 h-32 left-1/2 -ml-40 bottom-48 flex-row">
+        <View className="h-full bg-[#2F2F2F] w-36 items-center rounded-lg">
+          <View className="h-3/4 w-full p-2">
+            <View className="w-full h-full bg-green-400 rounded"></View>
+          </View>
+          <Text className="text-white">Sambong</Text>
         </View>
+        <View className="h-full bg-[#2F2F2F] w-36 items-center rounded-lg ml-4">
+          <View className="h-3/4 w-full p-2">
+            <View className="w-full h-full bg-green-600 rounded"></View>
+          </View>
+          <Text className="text-white">Ampalaya</Text>
+        </View>
+        <View className="h-full bg-[#2F2F2F] w-36 items-center rounded-lg ml-4">
+          <View className="h-3/4 w-full p-2">
+            <View className="w-full h-full bg-green-800 rounded"></View>
+          </View>
+          <Text className="text-white">Ampalaya</Text>
+        </View>
+      </View>
+
+      <TouchableOpacity className="z-10 absolute w-48 h-48 bg-[#1E1D1D] left-1/2 -ml-24 -bottom-4 rounded-full">
+        <View className="bg-transparent w-full items-center h-full mt-3">
+          <MaterialCommunityIcons name="image" size={25} color="#FFF"/>
+        </View>
+        
+      </TouchableOpacity>
+
+      <View className="z-30 absolute w-24 h-24 bg-[#090E05] left-1/2 bottom-8 -ml-12 rounded-full">
+
+      </View>
+      <TouchableOpacity className="z-40 absolute w-20 h-20 bg-emerald-400 left-1/2 bottom-10 -ml-10 rounded-full items-center justify-center">
+        <MaterialCommunityIcons name="camera" size={25} color="#000"/>
+      </TouchableOpacity>
+
+      <View className="z-20 absolute bottom-0 left-0 right-0 bg-[#2F2F2F] w-full h-20 rounded-full flex-row">
+        <TouchableOpacity className="w-1/5 bg-transparent items-center justify-center">
+          <MaterialCommunityIcons name="home-variant" size={30} color="#FFF"/>
+        </TouchableOpacity>
+        <TouchableOpacity className="w-1/5 bg-transparent items-center justify-center">
+          <MaterialCommunityIcons name="magnify" size={30} color="#FFF"/>
+        </TouchableOpacity>
+        <View className="w-1/5 bg-transparent">
+
+        </View>
+        <TouchableOpacity className="w-1/5 bg-transparent items-center justify-center">
+          <MaterialCommunityIcons name="line-scan" size={30} color="#FFF"/>
+        </TouchableOpacity>
+        <TouchableOpacity className="w-1/5 bg-transparent items-center justify-center">
+          <MaterialIcons name="person" size={30} color="#FFF"/>
+        </TouchableOpacity>
       </View>
       <StatusBar style='auto'/>
     </View>
@@ -164,10 +209,8 @@ const AccountScreen_Re = () => {
   )
 }
 
-const App = () => {
-  console.log(Date() + " - Compiled");
-
-  return (  
+const PostScanScreen_Re = () => {
+  return (
     <View>
       <View>
         <Text>Medium Size Halaman Picutre backgrounnd</Text>
@@ -196,7 +239,13 @@ const App = () => {
       </View>
       
     </View>
-  );
+  )
+}
+
+const App = () => {
+  console.log(Date() + " - Compiled");
+
+  return HomeScreen_Re()
 };
 
 export default App;

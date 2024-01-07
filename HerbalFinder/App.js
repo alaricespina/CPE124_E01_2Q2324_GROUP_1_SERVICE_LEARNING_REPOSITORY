5214889bee8bc4f68dcc757b47b6d6e9159e510d
Roger Angelo Salinas <radsalinas@mymail.mapua.edu.ph>
2024-01-07 15:11:47 +0800

    Update App.js

--- HerbalFinder/App.js
+++ HerbalFinder/App.js
@@ -366,6 +366,42 @@ const AccountScreen_Re = () => {
   )
 }
 
+const AboutUs_Re = () => {
+  return (
+    <View className="bg-[#090E05] w-full h-full">
+      <View className="flex-1 items-center justify-center">
+        {/* Inserted logo */}
+        <Image source={require('./pics/HBLogo.png')} style={{ width: 150, height: 120, marginBottom: 20 }} />
+
+        <Text className="text-white text-3xl font-bold mb-6">About Us</Text>
+        <View className="w-4/5 bg-[#2F2F2F80] rounded-2xl p-8 mb-[20%]">
+          <Text className="text-white text-base mb-4">
+            Welcome to our Herbal Finder App! We are here to provide valuable information
+            and promote the use of herbal plants.
+          </Text>
+          <Text className="text-white text-base mb-4">
+            Our mission is to educate users about various herbal plants, their benefits,
+            and how they can be used for different purposes, especially in natural remedies.
+          </Text>
+          <Text className="text-white text-base mb-4">
+            Do you have any questions or concerns? Feel free to reach out to us at HerbalNgInaMo@gmail.com.
+          </Text>
+        </View>
+      </View>
+
+      {/* "Back to Home" button with darker green color */}
+      <TouchableOpacity
+        className="z-40 absolute w-full h-16 bg-[#008000] bottom-0 items-center justify-center"
+      >
+        <Text className="text-white text-lg font-bold">Back to Home</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+
+
+
 const PostScanScreen_Re = () => {
   return (
     <View>
@@ -402,7 +438,7 @@ const PostScanScreen_Re = () => {
 const App = () => {
   console.log(Date() + " - Compiled");
 
-  return AccountScreen_Re()
+  return AboutUs_Re()
 };
 
 export default App;

15f79a81e9e7b177522d6555e0d93ceb9a058acb
alaricespina <alaricsespina@gmail.com>
2024-01-15 17:16:39 +0800

    H1

--- HerbalFinder/page_designs/Home.js
+++ HerbalFinder/page_designs/Home.js
@@ -2,28 +2,74 @@ import { View, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 const HomeScreen = () => {
-    console.log("At the Home screen")
     return (
-        <View className = "flex-1 bg-sky-700"> 
-            <Text className = "mt-4"> Home Screen </Text>
-            <View className = "w-full h-1/6 bg-sky-50">
-                
-            </View>
-            <Text> View Recent Pictures for Captures </Text>
-            <Text> Each recent picture would have their own name </Text>
-            <Text> Each recent picture can be opened </Text>
-            <View className = "w-full h-12 bg-sky-50 mt-12">
-
-            </View>
-            <Text> View Recent Plant Searches </Text>
-            <View className = "w-full h-12 bg-sky-50 mt-12">
-
-            </View>
-            <Text> View Recent Speech Searches </Text>
-            <StatusBar style="auto"/>
+      <>
+        <View className="absolute bg-green-800 w-full h-2/5 rounded-3xl" >
         </View>
+  
+        <View className="absolute w-80 top-16 left-1/2 -ml-40 flex-row ">
+          <View className="ml-1 mr-6 h-full items-center justify-center">
+            <MaterialCommunityIcons name="menu" size={25} color="#FFF"/>
+          </View>      
+          <Text className="h-full text-white">Hi Hatdog!</Text>
+          <View className="absolute h-full right-12 items-center justify-center">
+            <MaterialCommunityIcons name="line-scan" size={25} color="#FFF"/>
+          </View>
+          
+        </View>
+  
+        <View className="absolute w-80 h-12 top-32 left-1/2 -ml-40">   
+          <View className="absolute h-full items-center justify-center ml-2">
+            <MaterialCommunityIcons  name="magnify" size={30} color="#FFF"/>
+          </View>
+          <TextInput
+          placeholder='Search'
+          placeholderTextColor="white"
+          className = "absolute border-2 w-full h-full border-white rounded-lg pl-12 text-white"
+          />
+        </View>
+  
+        <View className="absolute w-80 left-1/2 -ml-40 top-52">
+          <Text className="text-white"> Know more about Herbal Plants </Text>
+        </View>
+  
+        <View className="absolute w-80 left-1/2 -ml-40 top-64 h-24 flex-row">
+          <View className="w-24 bg-emerald-500 h-full rounded-lg"></View>
+          <View className="w-24 bg-emerald-600 h-full rounded-lg ml-4"></View>
+          <View className="w-24 bg-emerald-700 h-full rounded-lg ml-4"></View>
+        </View>
+  
+        <View className="absolute w-80 left-1/2 -ml-40 top-96">
+          <Text className="text-white"> Popular Herbal Plants</Text>
+        </View>
+  
+        <View className="absolute w-80 h-32 left-1/2 -ml-40 bottom-48 flex-row">
+          <View className="h-full bg-[#2F2F2F] w-36 items-center rounded-lg">
+            <View className="h-3/4 w-full p-2">
+              <View className="w-full h-full bg-green-400 rounded"></View>
+            </View>
+            <Text className="text-white">Sambong</Text>
+          </View>
+          <View className="h-full bg-[#2F2F2F] w-36 items-center rounded-lg ml-4">
+            <View className="h-3/4 w-full p-2">
+              <View className="w-full h-full bg-green-600 rounded"></View>
+            </View>
+            <Text className="text-white">Ampalaya</Text>
+          </View>
+          <View className="h-full bg-[#2F2F2F] w-36 items-center rounded-lg ml-4">
+            <View className="h-3/4 w-full p-2">
+              <View className="w-full h-full bg-green-800 rounded"></View>
+            </View>
+            <Text className="text-white">Ampalaya</Text>
+          </View>
+        </View>
+  
+        
+        
+        
+      </>
     )
-    
-}
+  }
+  
 
 export default HomeScreen;

f680dc7a5f8f6bfa8c37a3502fa2ebccfeb5ad9d
alaricespina <alaricsespina@gmail.com>
2024-01-15 17:13:21 +0800

    2124125

--- HerbalFinder/App.js
+++ HerbalFinder/App.js
@@ -191,16 +191,6 @@ const ScannerScreen_Re = (...args) => {
   );
 };
 
-const HandleAccountButtonsPressed = (index, _setters, _ac_set) => {
-  console.log("Account Button Pressed")
-  _ac_set(false)
-  _setters[0](false)
-  _setters[1](false)
-  _setters[2](false)
-  _setters[index](true)
-
-}
-
 const AccountScreen_Re = (...args) => {
   var [SetActiveScreen] = args[0]
 
@@ -460,67 +450,6 @@ const PostScanScreen_Re = () => {
   )
 }
 
-
-const handleMenuButtonsPressed = (...args) => {
-  var [SetActiveScreen, targettedAttribute] = args[0]
-  var dSS = {...defaultScreenStates}
-  dSS[targettedAttribute] = true 
-  SetActiveScreen({...dSS})
-}
-
-const handleScreenDisplay = (...args) => {
-  
-  var [ActiveScreen, SetActiveScreen, s_SwitchStates, set_s_SwitchStates, DataObjects, SetDataObjects] = args[0]
-
-  // const Screens = {
-  //   Home : HomeScreen_Re(),
-  //   Search : SearchScreen_Re(),
-  //   Scanner : ScannerScreen_Re([DataObjects, SetDataObjects]),
-  //   AccountBase : AccountScreen_Re([SetActiveScreen]),
-  //   AccountProfile : (<></>),
-  //   AccountSettings : SettingScreen_Re([SetActiveScreen, s_SwitchStates, set_s_SwitchStates]),
-  //   AccountAbout : (<></>)
-  // }
-
-  // const corresponding_screen = Object.keys(ActiveScreen).filter(key => ActiveScreen[key])
-
-  // return Screens[corresponding_screen]
-  
-
-  if (ActiveScreen.Home) {
-    return HomeScreen_Re()
-  } else if (ActiveScreen.Search) {
-    return SearchScreen_Re()
-  } else if (ActiveScreen.Scanner) {
-    return ScannerScreen_Re([DataObjects, SetDataObjects])
-  } else if (ActiveScreen.AccountBase) {
-    return AccountScreen_Re([SetActiveScreen])
-  } else if (ActiveScreen.AccountSettings) {
-    return SettingScreen_Re([SetActiveScreen, s_SwitchStates, set_s_SwitchStates])
-  } else if (ActiveScreen.AccountAbout) {
-    return AboutUs_Re([SetActiveScreen])
-  
-  // } else if (scan) {
-  //   return ScannerScreen_Re(camera_params)
-  // } else if (account[0]) {
-  //   console.log("Displaying Account Screen")
-  //   return AccountScreen_Re(_account_states, _account_setters, account[1])
-  // } else if (_account_states[0]){
-  //   console.log("Displaying Setting Screen")
-  //   console.log("==========")
-  //   return SettingScreen_Re(settings_params)
-  // } else if (_account_states[1]){
-  //   return (<></>)
-  // } else if (_account_states[2]){
-  //   console.log("Displaying About Us Screen")
-  //   console.log("==========")
-  //   return AboutUs_Re(about_params)
-  } else {
-    return (<></>)
-  }
-  
-}
-
 const MaterialCommunityGradientIcon = (iconName, gradientStart, gradientEnd, defaultColor, monitoringVariable) => {
   if (monitoringVariable == true) {
     return (
@@ -579,7 +508,7 @@ const MaterialGradientIcon = (iconName, gradientStart, gradientEnd, defaultColor
   }
 }
 
-const BigAssCircle = () => {
+const MenuBarGalleryCircle = () => {
   return (
     <>
       <TouchableOpacity className="z-10 absolute w-[calc(200/375*100%)] left-[calc(50%-200/375/2*100%)] aspect-square bg-[#1E1D1D] bottom-[-4%] rounded-full">
@@ -592,26 +521,6 @@ const BigAssCircle = () => {
     </>
   )
 }
-
-const take_picture = async (camera) => {
-  if (camera) {
-    const data = await camera.takePictureAsync(null);
-    console.log(data.uri);
-    await MediaLibrary.saveToLibraryAsync(data.uri)
-    console.log("data saved")
-  }
-}
-
-const handleCameraPressed = (...args) => {
-  var [ActiveScreen, SetActiveScreen, targettedAttribute, DataObjects] = args[0]
-  if (ActiveScreen.Scanner) {
-    console.log("Say Cheese")
-    // take_picture(DataObject.CameraObj)
-  } else {
-    handleMenuButtonsPressed([SetActiveScreen, targettedAttribute])
-  }
-}
-
 
 const NavBar = (...args) => {
   var [ActiveScreen, SetActiveScreen, DataObjects, SetDataObjects] = args[0]
@@ -676,12 +585,6 @@ const NavBar = (...args) => {
     </>
   }
   
-}
-
-const displayObjectFull = (targetObject) => {
-  for (var x in targetObject) {
-    console.log(x + " : " + targetObject[x])  
-  }
 }
 
 const App = () => {

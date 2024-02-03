a8761d30374eb533024b2348e3d3e32b8f3131cd
Khyle Matthew Gopole <khygopole@gmail.com>
2024-02-03 21:21:00 +0800

    Update tryModel2.py

--- NLP_Herbal_MultilabelClassification/tryModel2.py
+++ NLP_Herbal_MultilabelClassification/tryModel2.py
@@ -3,22 +3,22 @@ import torch
 import numpy as np
 
 model_path = "khygopole/NLP_HerbalMultilabelClassification"
-inputText = input("Input your symptom to get the herbal prediction: ")
-
 model = AutoModelForSequenceClassification.from_pretrained(model_path)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+inputText = input("Input your symptom to get the herbal prediction: ")
 
 encoding = tokenizer(inputText, return_tensors="pt")
 encoding = {k: v.to(model.device) for k,v in encoding.items()}
 outputs = model(**encoding)
 logits = outputs.logits
-# apply sigmoid + threshold
+#Apply Sigmoid and Set Thresholding for Probabilities
 sigmoid = torch.nn.Sigmoid()
 probs = sigmoid(logits.squeeze().cpu())
 predictions = np.zeros(probs.shape)
 predictions[np.where(probs >= 0.5)] = 1
 
-# turn predicted id's into actual label names
+#Convert Predicted Labels into Actual Names of Herbal
 ans_cols = ["JACKFRUIT", "SAMBONG", "LEMON", "JASMINE", "MANGO", "MINT", "AMPALAYA", "MALUNGGAY", "GUAVA", "LAGUNDI"]
 predicted_labels = [ans_cols[idx] for idx, label in enumerate(predictions) if label == 1.0]
 print(predicted_labels)
